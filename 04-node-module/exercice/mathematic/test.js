38e60813a86952e627cb562ba3be980f743f0725
isi <isi@751FJW2>
2022-12-22 09:09:01 -0500

    mathematic module

--- 04-node-module/exercice/mathematic/test.js
+++ 04-node-module/exercice/mathematic/test.js
@@ -4,7 +4,7 @@ const mathematic = require ('./src/mathematic')
 
 
 const a=8, b=4
-console.log(a +" + "+ b+  " = " + mathematic.add(a,b))
+console.log(a +"  + "+ b+  " = " + mathematic.add(a,b))
 console.log(a +"  - "+ b+  " = " + mathematic.sub(a,b))
 console.log(a +"  / "+ b+  " = " + mathematic.div(a,b))
 console.log(a +"  * "+ b+  " = " + mathematic.mul(a,b))
