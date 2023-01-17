182e0f0920cf195226d555dc15ce9a0ef78ddd0f
isi <isi@751FJW2>
2023-01-17 09:45:03 -0500

    03-js-react-image-list

--- 10-react-async/exercice/03-js-react-image-list/src/component/image-list-component.jsx
+++ 10-react-async/exercice/03-js-react-image-list/src/component/image-list-component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ImageListComponent = ({ index, src }) => (
     <li id={index}>
-        <img src={'../image/' + src} alt={src} />
+        <img src={'image/' + src} alt={src} />
     </li>
 )
 
